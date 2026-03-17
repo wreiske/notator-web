7dc7fbef62465c64f01b17c8dca84a935c92f5a0
William Reiske <wreiske@mieweb.com>
2026-03-16 19:50:53 -0700

    fix: track WASM output files for Cloudflare Pages build
    The lib/son-parser-wasm/.gitignore had a wildcard '*' that prevented
    all WASM output files (.js, .wasm, .d.ts) from being committed.
    Without these files, the Cloudflare Pages build fails with:
      Cannot find module '@/lib/son-parser-wasm/son_parser'
    
    Now tracking: son_parser.js, son_parser.d.ts, son_parser_bg.wasm,
    son_parser_bg.wasm.d.ts, package.json. The .gitignore now only
    ignores build intermediates (target/, pkg/).

---
+++ lib/son-parser-wasm/son_parser.d.ts
@@ -0,0 +1,48 @@
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * Export a .SON file to Standard MIDI File bytes.
+ */
+export function export_midi_wasm(data: Uint8Array, song_name: string): Uint8Array;
+
+/**
+ * Parse a .SON file and return the SongData as a JS object.
+ */
+export function parse_son_wasm(data: Uint8Array): any;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+    readonly memory: WebAssembly.Memory;
+    readonly export_midi_wasm: (a: number, b: number, c: number, d: number) => [number, number, number, number];
+    readonly parse_son_wasm: (a: number, b: number) => [number, number, number];
+    readonly __wbindgen_malloc: (a: number, b: number) => number;
+    readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+    readonly __wbindgen_externrefs: WebAssembly.Table;
+    readonly __externref_table_dealloc: (a: number) => void;
+    readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+    readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
+/**
+ * Instantiates the given `module`, which can either be bytes or
+ * a precompiled `WebAssembly.Module`.
+ *
+ * @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+ *
+ * @returns {InitOutput}
+ */
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+ * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+ * for everything else, calls `WebAssembly.instantiate` directly.
+ *
+ * @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+ *
+ * @returns {Promise<InitOutput>}
+ */
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
